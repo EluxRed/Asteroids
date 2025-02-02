0cb9b674ba0f167d0dd517db4771cd53d141f855
eluxred <elux.red25@gmail.com>
2025-02-02 21:34:14 +0100

    limit FPS to 60

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    black = (0, 0, 0,)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:  
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return      
-        screen.fill(black)
-        pygame.display.flip()
+                return     
+        screen.fill("black")
+        pygame.display.flip() # refresh screen
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        # limit FPS to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
