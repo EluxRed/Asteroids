54f8c5bca3ae59d31a01fb089e7eb23164f6ab5e
eluxred <elux.red25@gmail.com>
2025-02-04 21:42:31 +0100

    added groups

--- main.py
+++ main.py
@@ -6,19 +6,38 @@ def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    # creates groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # adds Player class to both groups
+    Player.containers = (updatable, drawable)
+
+    # spawns the player object in the middle of the screen
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    # frequency
     dt = 0
 
+    # game loop
     while True:  
+        # makes me able to close the window providing a rerun value for clicking the QUIT button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return     
-        player.update(dt)
-        screen.fill("black")
-        player.draw(screen) # draws the player
-        pygame.display.flip() # refresh screen
+            
+        updatable.update(dt) # iterates automatically over evvery object in the group and applies their update method
 
-        # limit FPS to 60
+        screen.fill("black")
+
+        # draws the every object in the group
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip() # refreshes the screen
+
+        # limits FPS to 60
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
